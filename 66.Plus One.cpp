dd7cef7663674c3c7e426d617231e18dd7d65e22
Jie-Yu Chen <jieyuu174@gmail.com>
2024-10-16 14:04:11 +0800

    Update 66.Plus One.cpp

--- 66.Plus One.cpp
+++ 66.Plus One.cpp
@@ -3,12 +3,13 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         digits.at(digits.size() - 1) += 1;
 
-        for(vector<int>::iterator it = digits.end() - 1; it != digits.begin() - 1; --it){
+        for(vector<int>::iterator it = digits.end() - 1; it != digits.begin() - 1; it--){
             if(*it >= 10){
                 *it -= 10;
                 if(it == digits.begin()){
                     digits.emplace(digits.begin(), 1);
-                    continue;
+                    // 沒有 break 的話可能會在下一輪迴圈使用 it -1 造成越界讀取
+                    break;
                 }
                 *(it - 1) += 1; 
             }
