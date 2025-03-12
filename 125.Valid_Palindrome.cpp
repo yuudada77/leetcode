78cf6d075a5e3297e91adc395365c660b2ee0a4a
yuudada77 <jieyuu174@gmail.com>
2025-03-12 22:59:13 +0800

    250312_Add

---
+++ 125.Valid_Palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        bool isPalindrome(string s) {
+            string s1;
+    
+            for(char c : s){
+                if(isalnum(c)){
+                    s1 += tolower(c);
+                }
+            }
+    
+            int left = 0;
+            int right = s1.size() - 1;
+            while(left < right){
+                if(s1[left] != s1[right]){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    };
