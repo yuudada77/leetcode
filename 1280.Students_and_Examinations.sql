d770ef66802f251053d1331b2c0bfb2e5910ed7a
yuudada77 <jieyuu174@gmail.com>
2025-03-27 23:31:06 +0800

    250327_Add

---
+++ 1280.Students_and_Examinations.sql
@@ -0,0 +1,6 @@
+SELECT stu.student_id, stu.student_name, sub.subject_name, COUNT(exam.student_id) as attended_exams
+FROM Students stu
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations exam ON stu.student_id = exam.student_id AND sub.subject_name = exam.subject_name
+GROUP BY stu.student_id, stu.student_name, sub.subject_name
+ORDER BY stu.student_id, sub.subject_name;
